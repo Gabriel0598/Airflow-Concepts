53979f80248cd769d8c914e83b8d0a8b1baab332
Gabriel0598 <augustogabriel35@gmail.com>
2024-05-31 12:46:43 -0300

    XCOM Dags functions

--- dags/xcom_dag2.py
+++ dags/xcom_dag2.py
@@ -1,39 +1,39 @@
 from airflow import DAG
 from airflow.operators.python import PythonOperator, BranchPythonOperator
 from airflow.operators.bash import BashOperator
- 
+
 from datetime import datetime
- 
+
 def _t1(ti):
     ti.xcom_push(key='my_key', value=42)
- 
+
 def _t2(ti):
     ti.xcom_pull(key='my_key', task_ids='t1')
 
-def branch(ti):
+def _branch(ti):
     value = ti.xcom_pull(key='my_key', task_ids='t1')
     if (value == 42):
         return 't2'
     return 't3'
- 
+
 with DAG("xcom_dag", start_date=datetime(2022, 1, 1), 
     schedule_interval='@daily', catchup=False) as dag:
- 
+
     t1 = PythonOperator(
         task_id='t1',
         python_callable=_t1
     )
 
     branch = BranchPythonOperator(
-        task_id='t1',
-        python_callable=_t1
+        task_id='branch',
+        python_callable=_branch
     )
- 
+
     t2 = PythonOperator(
         task_id='t2',
         python_callable=_t2
     )
- 
+
     t3 = BashOperator(
         task_id='t3',
         bash_command="echo ''"
@@ -41,7 +41,8 @@ with DAG("xcom_dag", start_date=datetime(2022, 1, 1),
 
     t4 = BashOperator(
         task_id='t4',
-        bash_command="echo ''"
+        bash_command="echo ''",
+        trigger_rule='none_failed_min_one_success'
     )
- 
-    t1 >> branch >> t2 >> t3 >> t4
+
+    t1 >> branch >> [t2, t3] >> t4
